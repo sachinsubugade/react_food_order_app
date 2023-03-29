8e484ec330708077d29fda8867453b15e701e454
Sachin Subugade <sachinrs1310@gmail.com>
2023-03-29 23:34:39 +0530

    useContext

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,12 @@ import React, { Fragment } from "react";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A dining table" />
